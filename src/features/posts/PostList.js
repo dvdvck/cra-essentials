3466e21b3e4d39e717f5133b78bb16f8419effec
David Retana <dvdvck@gmail.com>
2021-02-10 00:12:43 -0600

    EntityAdapter

--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -2,11 +2,23 @@ import React, { useEffect } from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {Link} from 'react-router-dom'
 import { PostAuthor } from './PostAuthor';
-import {fetchPosts, selectAllPosts} from './postsSlice';
+import {fetchPosts, selectPostById, selectPostIds} from './postsSlice';
+
+let PostExcerpt = ({postId})=>{
+  const post = useSelector(state=> selectPostById(state, postId))
+  return (
+    <article className="posts-excerpt" key={post.id}>
+      <h3>{post.title}</h3>
+      <p className="post-content">{post.content.substring(0,100)}</p>
+      <PostAuthor userId={post.user} />
+      <Link to={`/post/${post.id}`} className="button muted-button">View Post</Link>
+    </article>
+  )
+}
 
 export const PostList = () =>{
   const dispatch = useDispatch();
-  const posts = useSelector(selectAllPosts);
+  const posts = useSelector(selectPostIds);
   //obtener el status
   const postStatus = useSelector(state=> state.posts.status)
   const error =  useSelector(state=> state.posts.error)
@@ -21,13 +33,8 @@ export const PostList = () =>{
   if(postStatus === 'loading'){
     content = <div className="loader">loading...</div>
   }else if (postStatus === 'succeded'){
-    content = posts.map(post =>(
-      <article className="posts-excerpt" key={post.id}>
-      <h3>{post.title}</h3>
-      <p className="post-content">{post.content.substring(0,100)}</p>
-      <PostAuthor userId={post.user} />
-      <Link to={`/post/${post.id}`} className="button muted-button">View Post</Link>
-      </article>
+    content = posts.map(postId =>(
+      <PostExcerpt key={postId} postId={postId} />
     ));
   }else if(postStatus === 'error'){
     content = <div>{error}</div>
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,12 +1,12 @@
-import {createAsyncThunk, createSlice, nanoid} from '@reduxjs/toolkit'
+import {createAsyncThunk, createEntityAdapter, createSlice, nanoid} from '@reduxjs/toolkit'
 import {client} from '../../api/client'
-
+/* 
 const initialState = {
   posts:[],
   status: 'idle',
   error: null,
 }
-
+ */
 //thunk para consultar los posts
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async ()=>{
   const response = await client.get('/fakeApi/posts')
@@ -19,12 +19,24 @@ export const addNewPost = createAsyncThunk('posts/addNewPost',async (initialPost
   return response.post
 })
 
+const postsAdapter = createEntityAdapter({
+  sortComparer: (a, b)=> b.date.localeCompare(a.date)
+})
+/*
+Estructura:
+  posts: {
+    ids:[key, ...], entities:{key: object_entity, ...}
+  }
+*/
+const initialState = postsAdapter.getInitialState({status: 'idle', error: null})
+
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
 
   reducers:{
     postAdded: {
+      /* DEPRECATED */
       reducer(state, action){
         state.posts.push(action.payload);
       },
@@ -36,9 +48,12 @@ const postsSlice = createSlice({
     },
     postUpdated: (state, action) =>{
       const updatedPost = action.payload
-      const index = state.posts.findIndex(post=> post.id === updatedPost.id)
-      if(index !== -1){
-        state.posts.splice(index, 1, updatedPost)
+      const existingPost = state.entities[updatedPost.id]
+      // const index = state.posts.findIndex(post=> post.id === updatedPost.id)
+      if(existingPost){
+        existingPost.title = updatedPost.title
+        existingPost.content = updatedPost.content
+        // state.posts.splice(index, 1, updatedPost)
       }
     }
   },
@@ -50,23 +65,30 @@ const postsSlice = createSlice({
     },
     [fetchPosts.fulfilled]: (state, action)=>{
       state.status = 'succeded'
-      state.posts = state.posts.concat(action.payload)
+      postsAdapter.upsertMany(state, action.payload)
+      // state.posts = state.posts.concat(action.payload)
     },
     [fetchPosts.rejected]: (state, action)=>{
       state.status = 'failed'
       state.error = action.error.message
     },
 
-    [addNewPost.fulfilled]: (state, action)=>{
+    [addNewPost.fulfilled]: postsAdapter.addOne
+/*     (state, action)=>{
       state.posts.push(action.payload)
-    }
+    } */
   }
 });
 
 export const {postAdded, postUpdated} = postsSlice.actions;
 export default postsSlice.reducer
-
+/* 
 export const selectAllPosts = state=> state.posts.posts
 export const selectPostById = postId=> state =>
   state.posts.posts.find(post=> post.id === postId)
-
+ */
+export const {
+  selectAll: selectAllPosts,
+  selectById: selectPostById,
+  selectIds: selectPostIds
+} = postsAdapter.getSelectors(state=> state.posts)
